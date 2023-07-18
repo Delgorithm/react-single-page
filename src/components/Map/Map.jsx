df549fd5caba45a999fd6b61b5e093adf3b0b687
Delgorithm <delgorithm@gmail.com>
2023-07-18 17:43:37 +0200

    new component : map

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Map.css';
+
+const Map = () => {
+  return (
+    <div className='map'>
+        <span className="background">
+            <div className="data-section">
+                <div className="data-first">
+                    <p>20+</p>
+                    <p>Years Experience</p>
+                </div>
+                <div className="data-second">
+                    <p>100k+</p>
+                    <p>Our Customers</p>
+                </div>
+                <div className="data-third">
+                    <p>100+</p>
+                    <p>Team</p>
+                </div>
+            </div>
+        </span>
+    </div>
+  )
+}
+
+export default Map
