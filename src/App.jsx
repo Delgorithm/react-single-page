d155e9dcc7360fc313fd9bdf684d3663e82b37c1
Delgorithm <delgorithm@gmail.com>
2023-07-18 17:39:40 +0200

    new component : map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Map from './components/Map/Map';
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Hero />
+            <Map />
         </div>
     );
 }
