9fa9fb3ef438c2c702c9ac22efb95fe86ccde50e
Delgorithm <delgorithm@gmail.com>
2023-07-18 22:02:52 +0200

    component : pricing

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -9,6 +9,10 @@ const Checkbox = ({ label }) => (
     </div>
 );
 
+const BtnStarted = () => (
+    <button className="btn">Get Started</button>
+)
+
 const Pricing = () => {
   return (
     <section className="price">
@@ -21,32 +25,35 @@ const Pricing = () => {
             </div>
         </div>
         <div className="trial-catalog">
-            <div className="first-catalog">
+            <div className="catalog">
                 <p>Single</p>
-                <p className='trial-price'>$10<span>/monthly</span></p>
+                <p className='trial-price'>$10/monthly</p>
                 <p>Get the most complete hosting service at the lowest price.</p>
                 <Checkbox label="Up to 135.000 Visitors/Month" />
                 <Checkbox label="Unlimited SSD Disk Space" />
                 <Checkbox label="Unlimited Bandwith" />
                 <Checkbox label="Unlimited Database" />
+                <BtnStarted />
             </div>
-            <div className="second-catalog">
+            <div className="catalog">
                 <p>Premium</p>
-                <p className='trial-price'>$15<span>/monthly</span></p>
+                <p className='trial-price'>$15/monthly</p>
                 <p>Get the most complete hosting service at the lowest price.</p>
                 <Checkbox label="Up to 135.000 Visitors/Month" />
                 <Checkbox label="Unlimited SSD Disk Space" />
                 <Checkbox label="Unlimited Bandwith" />
                 <Checkbox label="Unlimited Database" />
+                <BtnStarted />
             </div>
-            <div className="third-catalog">
+            <div className="catalog">
                 <p>Business</p>
-                <p className='trial-price'>$20<span>/monthly</span></p>
+                <p className='trial-price'>$20/monthly</p>
                 <p>Get the most complete hosting service at the lowest price.</p>
                 <Checkbox label="Up to 135.000 Visitors/Month" />
                 <Checkbox label="Unlimited SSD Disk Space" />
                 <Checkbox label="Unlimited Bandwith" />
                 <Checkbox label="Unlimited Database" />
+                <BtnStarted />
             </div>
         </div>
     </section>
